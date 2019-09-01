bc4e948ba14bce647cfe78e1f7e77f3293b2ea6c
MinexAutomation <minex.automation@gmail.com>
2019-08-31 22:10:36 -0400

    Added stringly-typed paths sourced from System.Environment.

--- source/R5T.NetStandard.IO.Paths.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.NetStandard.IO.Paths.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.NetStandard.IO.Paths.Base/Code/Utilities.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace R5T.NetStandard.IO.Paths.Base
+{
+    /// <summary>
+    /// Operates on stringly-typed paths.
+    /// </summary>
+    public static class Utilities
+    {
+        /// <summary>
+        /// Gets the current directory path.
+        /// Uses <see cref="Environment.CurrentDirectory"/>.
+        /// </summary>
+        public static string CurrentDirectoryPathValue
+        {
+            get
+            {
+                var output = Environment.CurrentDirectory;
+                return output;
+            }
+        }
+
+        /// <summary>
+        /// Gets the current user's profile directory path.
+        /// Uses <see cref="Environment.SpecialFolder.UserProfile"/>.
+        /// </summary>
+        public static string UserProfileDirectoryPathValue
+        {
+            get
+            {
+                var output = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                return output;
+            }
+        }
+
+        public static string ExecutablePathCommandLineArgumentValue
+        {
+            get
+            {
+                var output = Environment.GetCommandLineArgs()[0];
+                return output;
+            }
+        }
+    }
+}
